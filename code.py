5000e2f47169f28c288179b8c31e619b50528b76
johntable0405 <133057754+johntable0405@users.noreply.github.com>
2023-05-27 16:45:44 -0400

    Update code.py
    Improve UI

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
 import os
 from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, ttk
 
-# A class that handles the main logic of the application
 class ImageTagger:
     def __init__(self):
         self.image_index = 0
         self.images = []
         self.root = tk.Tk()
         self.root.configure(background='black')
-        self.textbox = Text(self.root)
+        self.root.title("Image Tagger")
+        self.root.geometry("800x600")
+        self.style = ttk.Style()
+        self.style.theme_use("clam")
+
+        self.textbox = Text(self.root, bg='white', fg='black')
         self.panel = tk.Label(self.root, background='black')
 
     def open_folder(self):
@@ -54,22 +58,32 @@ class ImageTagger:
 
     def show_image(self):
         image = Image.open(self.images[self.image_index])
+        w_ratio = image.width / self.root.winfo_width()
+        h_ratio = image.height / (self.root.winfo_height() // 2)
+        ratio = max(w_ratio, h_ratio)
+
+        image = image.resize((int(image.width / ratio), int(image.height / ratio)), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
         self.panel.config(image=photo)
         self.panel.image = photo
         self.load_annotation()
 
     def run(self):
-        open_button = tk.Button(self.root, text="Open Folder", command=self.open_folder)
-        next_button = tk.Button(self.root, text="Next Image", command=self.next_image)
-        prev_button = tk.Button(self.root, text="Previous Image", command=self.previous_image)
-        open_button.grid(row=0, column=0)
-        prev_button.grid(row=1, column=0)
-        next_button.grid(row=1, column=1)
-        self.panel.grid(row=2, column=0, columnspan=2)
-        self.textbox.grid(row=3, column=0, columnspan=2)
-        self.root.mainloop()
+        open_button = ttk.Button(self.root, text="Open Folder", command=self.open_folder)
+        next_button = ttk.Button(self.root, text="Next Image", command=self.next_image)
+        prev_button = ttk.Button(self.root, text="Previous Image", command=self.previous_image)
 
+        open_button.grid(row=0, column=0, pady=10, padx=10, sticky="ew")
+        prev_button.grid(row=1, column=0, pady=10, padx=10, sticky="ew")
+        next_button.grid(row=1, column=1, pady=10, padx=10, sticky="ew")
+        self.panel.grid(row=2, column=0, columnspan=2, pady=10, padx=10)
+        self.textbox.grid(row=3, column=0, columnspan=2, pady=10, padx=10, sticky="nsew")
+
+        # Configure the row and column weights
+        self.root.grid_rowconfigure(3, weight=1)
+        self.root.grid_columnconfigure(1, weight=1)
+
+        self.root.mainloop()
 
 if __name__ == "__main__":
     tagger = ImageTagger()
